dd50eb7e8b031d8a3b745fe494c56a1423a93f3b
somer81 <sevinc.omer@gmail.com>
2021-01-20 21:37:38 +0300

    updated

--- Program.cs
+++ Program.cs
@@ -6,6 +6,32 @@ using System.Threading.Tasks;
 
 namespace Musteri
 {
+
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+
+
+    class MusteriManager
+    {
+        void Add(Musteri musteri)
+        {
+
+        }
+
+        void Update(Musteri musteri) { }
+
+        void Delete(int Id) { }
+
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
